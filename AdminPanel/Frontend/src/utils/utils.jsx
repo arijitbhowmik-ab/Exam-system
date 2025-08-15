309db1efa063bc567b87cd9991edd29c884c78a0
ARIJIT BHOWMIK <arijit0483@gmail.com>
2025-08-16 01:03:20 +0530

    add features

--- AdminPanel/Frontend/src/utils/utils.jsx
+++ AdminPanel/Frontend/src/utils/utils.jsx
@@ -1,3 +1,3 @@
 // export const BACKEND_URL = "https://exam-system-snowy.vercel.app"
-// export const BACKEND_URL = "https://exam-system-04ua.onrender.com"
-export const BACKEND_URL = "http://192.168.0.103:5000"
+export const BACKEND_URL = "https://exam-system-04ua.onrender.com"
+// export const BACKEND_URL = "http://192.168.0.103:5000"
